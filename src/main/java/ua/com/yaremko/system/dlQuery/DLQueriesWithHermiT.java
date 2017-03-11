d8044f975f52e86aa400a1ff2d69bae982ed6eb8
Solomka <sol.yaremko@gmail.com>
2017-03-11 07:42:23 +0200

    DLQuery requests

--- src/main/java/ua/com/yaremko/system/dlQuery/DLQueriesWithHermiT.java
+++ src/main/java/ua/com/yaremko/system/dlQuery/DLQueriesWithHermiT.java
@@ -72,6 +72,15 @@ import org.semanticweb.owlapi.util.SimpleShortFormProvider;
 
 public class DLQueriesWithHermiT {
 
+	/*
+	 * DLQuery example with data property
+	 * 
+	 * Pizza and  (hasCalorificContentValue value 723)
+	 * 
+	 * кількістьКредитів value "4.5"^^xsd:double
+	 * 
+	 * Предмет and кількістьГодин value "4.6"^^xsd:double
+	 */
 	private final static String koala = "<?xml version=\"1.0\"?>\n"
 			+ "<rdf:RDF xmlns:rdf=\"http://www.w3.org/1999/02/22-rdf-syntax-ns#\" xmlns:rdfs=\"http://www.w3.org/2000/01/rdf-schema#\" xmlns:owl=\"http://www.w3.org/2002/07/owl#\" xmlns=\"http://protege.stanford.edu/plugins/owl/owl-library/koala.owl#\" xml:base=\"http://protege.stanford.edu/plugins/owl/owl-library/koala.owl\">\n"
 			+ "  <owl:Ontology rdf:about=\"\"/>\n"
